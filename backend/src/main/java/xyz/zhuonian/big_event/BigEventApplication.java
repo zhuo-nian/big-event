e846e19d42831375fa76d2e2da0e6a7b3b1a26aa
zhuo-nian <zts63590@gmail.com>
2026-03-04 22:38:56 +0800

    后端：初步构建完成

--- backend/src/main/java/xyz/zhuonian/big_event/BigEventApplication.java
+++ backend/src/main/java/xyz/zhuonian/big_event/BigEventApplication.java
@@ -1,13 +1,13 @@
-package xyz.zhuonian.backend;
+package xyz.zhuonian.big_event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BigEventApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BigEventApplication.class, args);
 	}
 
 }
